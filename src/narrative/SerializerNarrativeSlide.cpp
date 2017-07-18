0f47e96121a8da89d96bb49ea6029090cc29d2b4
David Stephan <stephandavid3@gmail.com>
2017-07-18 15:32:07 -0700

    more spaces to tabs

--- src/narrative/SerializerNarrativeSlide.cpp
+++ src/narrative/SerializerNarrativeSlide.cpp
@@ -2,13 +2,14 @@
 
 #include "narrative/NarrativeSlide.h"
 
-REGISTER_OBJECT_WRAPPER( NarrativeSlide,
-                         new ::NarrativeSlide,
-                         ::NarrativeSlide,
-                         "osg::Object osg::Node osg::Group ::NarrativeSlide" )
+REGISTER_OBJECT_WRAPPER( 
+	NarrativeSlide,
+	new ::NarrativeSlide,
+	::NarrativeSlide,
+	"osg::Object osg::Node osg::Group ::NarrativeSlide" )
 {
-    ADD_MATRIXD_SERIALIZER(CameraMatrix, osg::Matrixd());
-    ADD_FLOAT_SERIALIZER(Duration, 15.0f);
+	ADD_MATRIXD_SERIALIZER(CameraMatrix, osg::Matrixd());
+	ADD_FLOAT_SERIALIZER(Duration, 15.0f);
 	ADD_BOOL_SERIALIZER(StayOnNode, false);
 	ADD_FLOAT_SERIALIZER(TransitionDuration, 4.0f);
 	ADD_IMAGE_SERIALIZER(Thumbnail, osg::Image, NULL);
