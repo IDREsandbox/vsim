12a47fbb8b33d1aa2023196e35dbf92d012c8d6b
David Stephan <stephandavid3@gmail.com>
2017-08-14 12:53:52 -0700

    basic er scroll box

---
+++ src/resources/ERScrollBox.cpp
@@ -0,0 +1,43 @@
+﻿#include "resources/ERScrollBox.h"
+
+ERScrollBox::ERScrollBox(QWidget * parent)
+	: HorizontalScrollBox(parent)
+{
+	
+	// initialize menus
+	m_slide_menu = new QMenu(tr("ER context menu"), this);
+	m_action_new = new QAction("New Embedded Resource", m_slide_menu);
+	m_action_delete = new QAction("Delete Embedded Resource", m_slide_menu);
+	m_action_edit = new QAction("Edit Embedded Resource", m_slide_menu);
+	m_action_open = new QAction("Open Embedded Resource", m_slide_menu);
+
+	m_slide_menu->addAction(m_action_new);
+	m_slide_menu->addAction(m_action_delete);
+	m_slide_menu->addAction(m_action_edit);
+	m_slide_menu->addAction(m_action_open);
+
+	// forward signals
+	connect(m_action_new, &QAction::triggered, this, &ERScrollBox::sNew);
+	connect(m_action_delete, &QAction::triggered, this, &ERScrollBox::sDelete);
+	connect(m_action_edit, &QAction::triggered, this, &ERScrollBox::sEdit);
+	connect(m_action_open, &QAction::triggered, this, &ERScrollBox::sOpen);
+
+	setMenu(m_slide_menu);
+	setItemMenu(m_slide_menu);
+}
+
+ERScrollBox::~ERScrollBox() {
+	
+}
+
+ScrollBoxItem * ERScrollBox::createItem(osg::Node * node)
+{
+	EResourcesNode *er = dynamic_cast<EResourcesNode*>(node);
+	if (er == nullptr) {
+		qWarning() << "ER scroll box insert new ER. Node" << node << "is not a ER, creating a null item anyway";
+		//return nullptr;
+	}
+	ERScrollItem *item = new ERScrollItem(er);
+	connect(item, &ERScrollItem::sDoubleClick, this, &ERScrollBox::sOpen);
+	return item;
+}
---
+++ src/resources/ERScrollItem.cpp
@@ -0,0 +1,37 @@
+﻿#include "resources/ERScrollItem.h"
+
+ERScrollItem::ERScrollItem()
+	: m_er(nullptr)
+{
+	ui.setupUi(this);
+}
+ERScrollItem::ERScrollItem(EResourcesNode *er) 
+	: ERScrollItem()
+{
+	setER(er);
+}
+
+void ERScrollItem::setER(EResourcesNode *er)
+{
+	if (m_er) disconnect(m_er, 0, this, 0);
+
+	m_er = er;
+	if (er == nullptr) return;
+
+	connect(er, &EResourcesNode::sChanged, this,
+		[this]() {ui.title->setText(QString::fromStdString(er->getTitle())); });
+
+	ui.title->setText(QString::fromStdString(er->getTitle()));
+
+}
+
+void ERScrollItem::widthFromHeight(int height)
+{
+	return height;
+}
+
+void ERScrollItem::mouseDoubleClickEvent(QMouseEvent * event)
+{
+	emit sDoubleClick();
+}
+
