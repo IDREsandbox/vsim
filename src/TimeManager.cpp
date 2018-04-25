7d730efad96d1dcc66363e7b444d49e64816d1f0
David Stephan <stephandavid3@gmail.com>
2018-04-25 14:39:29 -0700

    time manager wip

---
+++ src/TimeManager.cpp
@@ -0,0 +1,51 @@
+﻿#include "TimeManager.h"
+
+#include "ModelGroup.h"
+#include "Model.h"
+#include "VSimApp.h"
+#include "VSimRoot.h"
+
+TimeManager::TimeManager(VSimApp *app, QObject *parent)
+	: QObject(parent),
+	m_app(app)
+{
+	connect(m_app, &VSimApp::sAboutToReset, this, [this]() {
+
+	});
+	connect(m_app, &VSimApp::sReset, this, [this]() {
+		ModelGroup *models = m_app->getRoot()->models();
+	});
+}
+
+int TimeManager::year() const
+{
+	return m_year;
+}
+
+void TimeManager::setYear(int year)
+{
+	m_year = year;
+	emit sYearChanged(year);
+}
+
+bool TimeManager::timeEnabled() const
+{
+	return m_enabled;
+}
+
+void TimeManager::enableTime(bool enable)
+{
+	m_enabled = enable;
+	emit sTimeEnableChanged(enable);
+}
+
+std::set<int> TimeManager::keyYears() const
+{
+	return m_keys;
+}
+
+void TimeManager::setKeyYears(const std::set<int> &keys)
+{
+	m_keys = keys;
+	emit sKeysChanged();
+}
---
+++ src/TimeManager.h
@@ -0,0 +1,37 @@
+﻿#ifndef TIMEMANAGER_H
+#define TIMEMANAGER_H
+
+#include <QObject>
+#include <set>
+
+class VSimApp;
+class ModelGroup;
+
+class TimeManager : public QObject {
+    Q_OBJECT
+public:
+    TimeManager(VSimApp *app, QObject *parent = nullptr);
+
+    int year() const;
+    void setYear(int year);
+
+    bool timeEnabled() const;
+    void enableTime(bool enable);
+
+    std::set<int> keyYears() const;
+    void setKeyYears(const std::set<int> &keys);
+
+signals:
+	void sTimeEnableChanged(bool enabled);
+	void sYearChanged(int year);
+	void sKeysChanged();
+
+private:
+    VSimApp *m_app;
+    ModelGroup *m_models;
+    int m_year;
+    bool m_enabled;
+    std::set<int> m_keys;
+};
+
+#endif
