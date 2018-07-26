34a37d70e3f9bcc63d2a1c45d0427f3edcd8b218
David Stephan <stephandavid3@gmail.com>
2018-07-26 11:23:41 -0700

    added gui library, added TitledComboBox

---
+++ src/Gui/TitledComboBox.cpp
@@ -0,0 +1,29 @@
+﻿#include "TitledComboBox.h"
+
+#include <QPaintEvent>
+#include <QStylePainter>
+
+TitledComboBox::TitledComboBox(QWidget * parent)
+	: QComboBox(parent)
+{
+}
+
+void TitledComboBox::setTitle(const QString & title)
+{
+	m_title = title;
+}
+
+// straight up copied from Qt source
+// changed opt.currentText to be the same as title
+void TitledComboBox::paintEvent(QPaintEvent * e)
+{
+	QStylePainter painter(this);
+	painter.setPen(palette().color(QPalette::Text));
+
+	QStyleOptionComboBox opt;
+	initStyleOption(&opt);
+	opt.currentText = m_title;
+
+	painter.drawComplexControl(QStyle::CC_ComboBox, opt);
+	painter.drawControl(QStyle::CE_ComboBoxLabel, opt);
+}
---
+++ src/Gui/TitledComboBox.h
@@ -0,0 +1,18 @@
+﻿#ifndef TITLEDCOMBOBOX_H
+#define TITLEDCOMBOBOX_H
+
+#include <QComboBox>
+
+class TitledComboBox : public QComboBox {
+public:
+	TitledComboBox(QWidget *parent = nullptr);
+
+	void setTitle(const QString &title);
+
+protected:
+	void paintEvent(QPaintEvent *e) override;
+
+private:
+	QString m_title;
+};
+#endif
