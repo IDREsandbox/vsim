c1c14943a372f644573c8e703c3403b09d0f2569
David Stephan <stephandavid3@gmail.com>
2018-11-30 16:12:02 -0800

    help dialog WIP

---
+++ src/HelpDialog.cpp
@@ -0,0 +1,10 @@
+﻿#include "HelpDialog.h"
+
+#include <QDebug>
+
+HelpDialog::HelpDialog(QWidget *parent)
+	: QDialog(parent)
+{
+	ui.setupUi(this);
+	setWindowFlag(Qt::WindowContextHelpButtonHint, false);
+}
---
+++ src/HelpDialog.h
@@ -0,0 +1,17 @@
+﻿#ifndef HELPDIALOG_H
+#define HELPDIALOG_H
+
+#include "ui_HelpDialog.h"
+
+#include <QDialog>
+
+class HelpDialog : public QDialog {
+	Q_OBJECT;
+public:
+	HelpDialog(QWidget *parent = nullptr);
+
+private:
+	Ui::HelpDialog ui;
+};
+
+#endif
